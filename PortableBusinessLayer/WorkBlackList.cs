ad945605848452e80c373cffc1b50ce4681db4c5
correiadefreitas <correiadefreitas@gmail.com>
2016-05-24 15:59:49 +0100

    BL created

---
+++ PortableBusinessLayer/WorkBlackList.cs
@@ -0,0 +1,247 @@
+﻿using System;
+
+namespace QandA
+{
+    public class WordBlackList
+    {
+        private string[] blackListFull;
+        private string[] blackListPartial;
+        private string[] blackListExclusion;
+        private string s;
+
+        public readonly char[] WordBreakChars = { ' ', '\n', '\r', '\t', '\v', '\f' };
+
+        const string ReplaceChar = "*";
+        const string NotImplementedExceptionMsg = "List type not implemented.";
+
+        public enum wordListType
+        {
+            Partial,
+            Full,
+            Exclusion
+        }
+
+        public WordBlackList()
+        {
+        }
+
+        public void SetString(string s)
+        {
+            this.s = s;
+        }
+
+        public void SetList(string[] wordList, wordListType type)
+        {
+            switch (type)
+            {
+                case wordListType.Partial:
+                    {
+                        blackListPartial = wordList;
+                        break;
+                    }
+                case wordListType.Full:
+                    {
+                        blackListFull = wordList;
+                        break;
+                    }
+                case wordListType.Exclusion:
+                    {
+                        blackListExclusion = wordList;
+                        break;
+                    }
+                default:
+                    {
+                        throw new NotImplementedException(this.GetType().ToString() + ": SetList: " + NotImplementedExceptionMsg);
+                    }
+            }
+        }
+
+        public string Process()
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return "";
+            }
+
+            if (blackListPartial != null && blackListPartial.GetLength(0) > 0)
+            {
+                ProcessBlackList(wordListType.Partial);
+            }
+
+            if (blackListFull != null && blackListFull.GetLength(0) > 0)
+            {
+                ProcessBlackList(wordListType.Full);
+            }
+
+            return s;
+        }
+
+        private void ProcessBlackList(wordListType type)
+        {
+            string[] blackList = { };
+            switch(type)
+            {
+                case wordListType.Partial:
+                    blackList = blackListPartial;
+                    break;
+                case wordListType.Full:
+                    blackList = blackListFull;
+                    break;
+                default:
+                    throw new NotImplementedException(this.GetType().ToString() + ": SetList: " + NotImplementedExceptionMsg);
+            }
+
+            foreach (var bl in blackList)
+            {
+                var size = bl.Length;
+                if (size > 1)
+                {
+
+                    char start = bl[0];
+                    var startIndex = s.IndexOf(start);
+                    var restIndex = 0;
+                    int LetterIndex = 0;
+                    while (startIndex != -1)
+                    {
+                        restIndex = s.Length - startIndex;
+                        
+                        if (restIndex >= bl.Length)
+                        {
+                            LetterIndex = FindWord(bl, startIndex, restIndex);
+
+                            if (CheckSize(type, size, startIndex, LetterIndex))
+                            {
+                                int startOfString = startIndex;
+                                int endOfString = LetterIndex;
+
+                                string PartialWord = bl;
+
+                                PartialWord = GetComposedWord(startIndex, LetterIndex, type, ref startOfString, ref endOfString);
+
+                                if (IsToRemove(PartialWord))
+                                {
+                                    s = s.Remove(startOfString, endOfString).Insert(startOfString, new string(ReplaceChar[0], endOfString));
+                                }
+                            }
+                        }
+
+                        startIndex = s.IndexOf(start, startIndex + LetterIndex);
+
+                    }
+                    s.IndexOf(bl);
+                }
+                else
+                {
+                    s.Replace(bl, ReplaceChar);
+                }
+            }
+        }
+
+        private bool IsToRemove(string PartialWord)
+        {
+            var Remove = true;
+            if (blackListExclusion != null && blackListExclusion.Length > 0)
+            {
+                foreach (var exclusion in blackListExclusion)
+                {
+                    if (exclusion == PartialWord)
+                    {
+                        Remove = false;
+                        break;
+                    }
+                }
+            }
+
+            return Remove;
+        }
+
+        private bool CheckSize(wordListType type, int size, int startIndex, int LetterIndex)
+        {
+            bool checkSize = LetterIndex >= size;
+            if (type == wordListType.Full)
+            {
+                checkSize = checkSize &&
+                    (startIndex + LetterIndex == s.Length || s.Substring(startIndex + LetterIndex).StartsWith(" ")) &&
+                    (startIndex == 0 || s.Substring(startIndex - 1).StartsWith(" "));
+            }
+
+            return checkSize;
+        }
+
+        private string GetComposedWord(int startIndex, int LetterIndex, wordListType type, ref int startOfString, ref int endOfString)
+        {
+            int startOfPartialString = startOfString;
+            int endOfPartialString = endOfString;
+
+            for (int WordBreakCharsCounter = 0; WordBreakCharsCounter < WordBreakChars.Length - 1; WordBreakCharsCounter++)
+            {
+                startOfPartialString = s.Substring(0, startIndex).LastIndexOf(WordBreakChars[WordBreakCharsCounter]);
+                if (startOfPartialString != -1)
+                {
+                    startOfPartialString++;
+                    break;
+                }
+            }
+
+            for (int WordBreakCharsCounter = 0; WordBreakCharsCounter < WordBreakChars.Length - 1; WordBreakCharsCounter++)
+            {
+                endOfPartialString = s.Substring(startIndex + LetterIndex).IndexOf(WordBreakChars[WordBreakCharsCounter]);
+                if (endOfPartialString != -1)
+                {
+                    //EndOfString--;
+                    break;
+                }
+            }
+
+            //For start and end of string
+            startOfPartialString = startOfPartialString == -1 ? 0 : startOfPartialString;
+            endOfPartialString = endOfPartialString == -1 ? s.Length - startIndex : endOfPartialString + startIndex + LetterIndex - startOfPartialString;
+
+            if (type == wordListType.Partial)
+            {
+                startOfString = startOfPartialString;
+                endOfString = endOfPartialString;
+                return s.Substring(startOfString, endOfString);
+            }
+
+            if (type == wordListType.Full)
+            {
+                var PartialWord = s.Substring(startOfPartialString, endOfPartialString);
+                foreach(var word in WordBreakChars)
+                {
+                    return PartialWord.Replace(word.ToString(), "");
+                }
+            }
+
+            throw new NotImplementedException(this.GetType().ToString() + ": SetList: " + NotImplementedExceptionMsg);
+        }
+
+        private int FindWord(string bl, int startIndex, int restIndex)
+        {
+            var NextLetterIndexInString = 1;
+            var NextLetterIndexInBlackList = 1;
+            for (var l = startIndex + 1; l < s.Length + 1; l++)
+            {
+                if (NextLetterIndexInBlackList < bl.Length)
+                {
+                    var sub = s.Substring(l + NextLetterIndexInString - NextLetterIndexInBlackList, restIndex - NextLetterIndexInString);
+                    if (sub.TrimStart().Length != 0)
+                    {
+                        if (sub.TrimStart().StartsWith(bl[NextLetterIndexInBlackList].ToString()))
+                        {
+                            NextLetterIndexInString += sub.IndexOf(bl[NextLetterIndexInBlackList].ToString()) + 1;
+                            NextLetterIndexInBlackList++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+            return NextLetterIndexInString;
+        }
+    }
+}
--- TTDWorkBlackList/UnitTestWords.cs
+++ TTDWorkBlackList/UnitTestWords.cs
@@ -1,27 +1,20 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QandA;
 
-namespace TTDWordBlackList
+namespace WordBlackListTests
 {
     [TestClass]
     public class UnitTestWords
     {
-        const string text = "fucksuckfuck";
-        readonly static string[] fullList = { "fuck", "suck" };
-        const string text_fullList = "************";
+        const string text = "holyfuck you fucktard you s u c k ass and get fucked assimetric metric";
 
-        readonly static string[] partialList = { "ass" };
+        readonly static string[] fullList = { "metric" };
+        readonly static string[] partialList = { "fuck", "suck", "ass" };
         readonly static string[] excludedList = { "assimetric" };
 
-
-        //const string text = "holyfuck you fucktard you s u c k and get fucked";
-        //readonly static string[] fullList = { "fuck", "suck" };
-        //const string text_fullList = "******** you ******** you ******* and get ******";
-
-        //readonly static string[] partialList = { "ass" };
-        //readonly static string[] excludedList = { "assimetric" };
-
-
+        const string text_partial =  "******** you ******** you ******* *** and get ****** ********** metric";
+        const string text_fullList = "holyfuck you fucktard you s u c k ass and get fucked assimetric ******";
+        const string text_all =      "******** you ******** you ******* *** and get ****** assimetric ******";
 
         //const string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lacus urna, vestibulum quis risus hendrerit, venenatis volutpat quam. Donec iaculis facilisis nunc a imperdiet. In pretium felis in dictum mollis. Praesent facilisis imperdiet massa ac pretium. Mauris ante risus, accumsan vel facilisis non, porta ut lorem. Donec in nisi sap\nien. Nulla a vehicula mi, quis elementum sem. Nam est tortor, scelerisque at nisl nec, maximus accumsan mi. Proin tempus vestibulum justo sed volutpat. Nunc porta ligula a lacus laoreet volutpat. Donec lobortis nibh ac dui auctor interdum. Integer sed purus placerat, placerat ante at, accumsan massa. Phasellus semper magna eu pellentesque blandit.\nEtiam tempus faucibus facilisis.Integer diam urna, mattis nec neque sed, euismod lobortis mi.Phasellus et velit nunc. Vestibulum suscipit at lorem vitae ornare. Aenean in luctus elit. Integer eget est suscipit, suscipit felis id, sodales lacus. Etiam in laoreet mauris, id scelerisque libero.\nSed tellus orci, sodales scelerisque convallis et, fermentum ultrices tortor.Duis dignissim feugiat nisi ac fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Maecenas et dolor et ligula volutpat aliquam nec sit amet velit.Quisque vel velit blandit risus rhoncus posuere eget sit amet orci.Nam eget mi at odio tempus egestas interdum eget nullas. Donec vitae maximus est. Mauris egestas fringilla metus semper pulvinar. Integer egestas ullamcorper pretium. In eleifend leo at eros pellentesque, vitae facilisis leo dictum. Sed tincidunt egestas aliquam. Maecenas eu leo at nisi feugiat sollicitudin vitae vel leo. Aliquam ac mollis est. Proin venenatis mollis magna, nec tincidunt eros ornare a.Nulla maximus imperdiet lorem non dapibus.\nEtiam cursus id nisl ultrices tempor. Praesent imperdiet, arcu in pulvinar varius, augue erat aliquet est, id vulputate ligula ligula eget tellus. Vivamus porttitor mi metus, vel auctor nisl aliquet ac.Integer cursus vitae dolor eu lobortis. Praesent nec facilisis elit, at interdum turpis.Morbi consequat efficitur malesuada. Phasellus tristique lacus magna, suscipit finibus orci mollis id.Duis vitae justo sed risus sollicitudin semper.Etiam imperdiet nisl metus, sed porta leo scelerisque et. Donec at augue at libero pellentesque lobortis fringilla a orci. Maecenas sit amet enim mollis, hendrerit diam in, pulvinar velit.\nFusce rutrum sit amet eros eget pharetra. Nulla eleifend congue mollis. Fusce tristique ultrices risus, id laoreet mi hendrerit id.Quisque congue aliquet mauris. Integer cursus mattis sapien a condimentum. In eu dapibus justo. Pellentesque porttitor pulvinar cursus.";
 
@@ -32,21 +25,38 @@ namespace TTDWordBlackList
         //const string text_fullList = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lacus urna, vestibulum quis risus hendrerit, venenatis volutpat quam. Donec iaculis facilisis nunc a imperdiet. In pretium felis in dictum mollis. Praesent facilisis imperdiet massa ac pretium. Mauris ante risus, accumsan vel facilisis non, porta ut lorem. Donec in nisi ***\n***. ***** a vehicula mi, quis elementum sem. Nam est tortor, scelerisque at nisl nec, maximus accumsan mi. Proin tempus vestibulum justo sed volutpat. Nunc porta ligula a lacus laoreet volutpat. Donec lobortis nibh ac dui auctor interdum. Integer sed purus placerat, placerat ante at, accumsan massa. Phasellus semper magna eu pellentesque blandit.\nEtiam tempus faucibus facilisis.Integer diam urna, mattis nec neque sed, euismod lobortis mi.Phasellus et velit nunc. Vestibulum suscipit at lorem vitae ornare. Aenean in luctus elit. Integer eget est suscipit, suscipit felis id, sodales lacus. Etiam in laoreet mauris, id scelerisque libero.\nSed tellus orci, sodales scelerisque convallis et, fermentum ultrices tortor.Duis dignissim feugiat nisi ac fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Maecenas et dolor et ligula volutpat aliquam nec sit amet velit.Quisque vel velit blandit risus rhoncus posuere eget sit amet orci.Nam eget mi at odio tempus egestas interdum eget nullas. Donec vitae maximus est. Mauris egestas fringilla metus semper pulvinar. Integer egestas ullamcorper pretium. In eleifend leo at eros pellentesque, vitae facilisis leo dictum. Sed tincidunt egestas aliquam. Maecenas eu leo at nisi feugiat sollicitudin vitae vel leo. Aliquam ac mollis est. Proin venenatis mollis magna, nec tincidunt eros ornare a.***** maximus imperdiet lorem non dapibus.\nEtiam cursus id nisl ultrices tempor. Praesent imperdiet, arcu in pulvinar varius, augue erat aliquet est, id vulputate ligula ligula eget tellus. Vivamus porttitor mi metus, vel auctor nisl aliquet ac.Integer cursus vitae dolor eu lobortis. Praesent nec facilisis elit, at interdum turpis.Morbi consequat efficitur malesuada. Phasellus tristique lacus magna, suscipit finibus orci mollis id.Duis vitae justo sed risus sollicitudin semper.Etiam imperdiet nisl metus, sed porta leo scelerisque et. Donec at augue at libero pellentesque lobortis fringilla a orci. Maecenas sit amet enim mollis, hendrerit diam in, pulvinar velit.\nFusce rutrum sit amet eros eget pharetra. ***** eleifend congue mollis. Fusce tristique ultrices risus, id laoreet mi hendrerit id.Quisque congue aliquet mauris. Integer cursus mattis ****** a condimentum. In eu dapibus justo. Pellentesque porttitor pulvinar cursus.";
 
         [TestMethod]
+        public void NoStringNoLists()
+        {
+            WordBlackList wbl = new WordBlackList();
+            string p = wbl.Process();
+            Assert.AreEqual(p, "");
+        }
+
+
+        [TestMethod]
         public void NoStringBlackList()
         {
+            WordBlackList wbl = new WordBlackList();
+
             string[] BlackList = null;
-            WordBlackList wbl = new WordBlackList(BlackList);
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
             string p = wbl.Process();
+
             Assert.AreEqual(p, string.Empty);
         }
 
         [TestMethod]
         public void NoStringAllLists()
         {
+            WordBlackList wbl = new WordBlackList();
+
             string[] BlackList = fullList;
             string[] BlackListPartial = partialList;
             string[] BlackListExclusion = excludedList;
-            WordBlackList wbl = new WordBlackList(BlackList, BlackListPartial, BlackListExclusion);
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
+            wbl.SetList(BlackListPartial, WordBlackList.wordListType.Partial);
+            wbl.SetList(BlackListExclusion, WordBlackList.wordListType.Exclusion);
+
             string p = wbl.Process();
             Assert.AreEqual(p, string.Empty);
         }
@@ -54,8 +64,9 @@ namespace TTDWordBlackList
         [TestMethod]
         public void NoBlackList()
         {
+            WordBlackList wbl = new WordBlackList();
             string s = text;
-            WordBlackList wbl = new WordBlackList(s);
+            wbl.SetString(s);
             string p = wbl.Process();
             Assert.AreEqual(p, text);
         }
@@ -63,9 +74,11 @@ namespace TTDWordBlackList
         [TestMethod]
         public void NullString()
         {
-            string[] BlackList = fullList;
+            WordBlackList wbl = new WordBlackList();
             string s = null;
-            WordBlackList wbl = new WordBlackList(s, BlackList);
+            wbl.SetString(s);
+            string[] BlackList = fullList;
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
             string p = wbl.Process();
             Assert.AreEqual(p, string.Empty);
         }
@@ -73,9 +86,11 @@ namespace TTDWordBlackList
         [TestMethod]
         public void EmptyString()
         {
-            string[] BlackList = fullList;
+            WordBlackList wbl = new WordBlackList();
             string s = string.Empty;
-            WordBlackList wbl = new WordBlackList(s, BlackList);
+            wbl.SetString(s);
+            string[] BlackList = fullList;
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
             string p = wbl.Process();
             Assert.AreEqual(p, string.Empty);
         }
@@ -83,9 +98,11 @@ namespace TTDWordBlackList
         [TestMethod]
         public void NullBlackList()
         {
-            string[] BlackList = null;
+            WordBlackList wbl = new WordBlackList();
             string s = text;
-            WordBlackList wbl = new WordBlackList(s, BlackList);
+            wbl.SetString(s);
+            string[] BlackList = null;
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
             string p = wbl.Process();
             Assert.AreEqual(p, text);
         }
@@ -93,9 +110,11 @@ namespace TTDWordBlackList
         [TestMethod]
         public void EmptyBlackList()
         {
-            string[] BlackList = new string[] { };
+            WordBlackList wbl = new WordBlackList();
             string s = text;
-            WordBlackList wbl = new WordBlackList(s, BlackList);
+            wbl.SetString(s);
+            string[] BlackList = new string[] { };
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
             string p = wbl.Process();
             Assert.AreEqual(p, text);
         }
@@ -104,9 +123,11 @@ namespace TTDWordBlackList
         [TestMethod]
         public void FullWords()
         {
-            string[] BlackList = fullList;
+            WordBlackList wbl = new WordBlackList();
             string s = text;
-            WordBlackList wbl = new WordBlackList(s, BlackList);
+            wbl.SetString(s);
+            string[] BlackList = fullList;
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
             string p = wbl.Process();
             Assert.AreEqual(p, text_fullList);
         }
@@ -114,37 +135,43 @@ namespace TTDWordBlackList
         [TestMethod]
         public void PartialWords()
         {
-            string[] BlackList = new string[] { };
-            string[] BlackListPartial = partialList;
-            string[] BlackListExclusion = new string[] { };
+            WordBlackList wbl = new WordBlackList();
             string s = text;
-            WordBlackList wbl = new WordBlackList(s, BlackList, BlackListPartial, BlackListExclusion);
+            wbl.SetString(s);
+            string[] BlackList = partialList;
+            wbl.SetList(BlackList, WordBlackList.wordListType.Partial);
             string p = wbl.Process();
-            Assert.AreEqual(p, "");
+            Assert.AreEqual(p, text_partial);
         }
 
         [TestMethod]
         public void Exclusions()
         {
-            string[] BlackList = new string[] { };
-            string[] BlackListPartial = partialList;
-            string[] BlackListExclusion = excludedList;
+            WordBlackList wbl = new WordBlackList();
             string s = text;
-            WordBlackList wbl = new WordBlackList(s, BlackList, BlackListPartial, BlackListExclusion);
+            wbl.SetString(s);
+            string[] BlackList = fullList;
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
+            string[] BlackListExlusinons = excludedList;
+            wbl.SetList(excludedList, WordBlackList.wordListType.Exclusion);
             string p = wbl.Process();
-            Assert.AreEqual(p, "");
+            Assert.AreEqual(p, text_fullList);
         }
 
         [TestMethod]
         public void AllLists()
         {
-            string[] BlackList = fullList;
-            string[] BlackListPartial = partialList;
-            string[] BlackListExclusion = excludedList;
+            WordBlackList wbl = new WordBlackList();
             string s = text;
-            WordBlackList wbl = new WordBlackList(s, BlackList, BlackListPartial, BlackListExclusion);
+            wbl.SetString(s);
+            string[] BlackList = fullList;
+            wbl.SetList(BlackList, WordBlackList.wordListType.Full);
+            string[] BlackListPartial = partialList;
+            wbl.SetList(BlackListPartial, WordBlackList.wordListType.Partial);
+            string[] BlackListExlusinons = excludedList;
+            wbl.SetList(excludedList, WordBlackList.wordListType.Exclusion);
             string p = wbl.Process();
-            Assert.AreEqual(p, "");
+            Assert.AreEqual(p, text_all);
         }
     }
 }
